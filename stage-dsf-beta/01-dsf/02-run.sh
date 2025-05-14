e736b1c36d1939b138fdf88d98082fbd628d877c
ramengo <angelocantalp@gmail.com>
2025-05-14 14:34:38 +0200

    update stage dsf

--- stage-dsf-beta/01-dsf/02-run.sh
+++ stage-dsf-beta/01-dsf/02-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-install -m 644 files/config.g -o 996 -g 996 "${ROOTFS_DIR}/opt/dsf/sd/sys/config.g"
-install -m 644 files/plugins.txt -o 996 -g 996 "${ROOTFS_DIR}/opt/dsf/conf/plugins.txt"
+install -m 644 files/config.g -o 992 -g 992 "${ROOTFS_DIR}/opt/dsf/sd/sys/config.g"
+install -m 644 files/plugins.txt -o 992 -g 992 "${ROOTFS_DIR}/opt/dsf/conf/plugins.txt"
 
 on_chroot << EOF
 systemctl enable duetcontrolserver
@@ -9,6 +9,9 @@ systemctl enable duetwebserver
 systemctl enable duetpluginservice
 systemctl enable duetpluginservice-root
 
-gpasswd -a pi dsf
-gpasswd -a dsf video
+adduser $FIRST_USER_NAME dsf
+adduser dsf video
+
+chown dsf:dsf /opt/dsf/sd/sys/config.g
+chown dsf:dsf /opt/dsf/conf/plugins.txt
 EOF
