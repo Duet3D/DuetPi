a50881e85d062287cc6483426e6f357301753d92
ramengo <angelocantalp@gmail.com>
2026-01-27 11:01:58 +0100

    update clear image

--- clean_image.sh
+++ clean_image.sh
@@ -1,3 +1,5 @@
 rm /work/ElementoTC/export-image/*.img
 rm /work/ElementoTC-beta/export-image/*.img
 rm /work/UM2/export-image/*.img
+rm /work/3dforme/export-image/*.img
+rm /work/Elemento5TC/export-image/*.img
